9ffd4a78edcfe0647b3542837297d80c0e55f0b5
UveGG <uveuve73@mail.ru>
2019-02-11 20:30:09 +0300

    WellDone
    tududududu

--- StickmanWar.py
+++ StickmanWar.py
@@ -27,6 +27,7 @@ def screen_update():
 
     unused_sprites.update()
     fighter_sprites.update()
+    unused_sprites.draw(screen)
     fighter_sprites.draw(screen)
 
     pygame.display.flip()
@@ -69,17 +70,34 @@ class Fighter1(pygame.sprite.Sprite):
 
     def __init__(self, group):
         super().__init__(group)
+        self.falling = 0
         self.image = Fighter1.images['standing'][0]
         self.rect = self.image.get_rect()
         self.width, self.height = self.rect[2], self.rect[3]
         self.rect.x, self.rect.y = 100, 100
         self.direction = 'right'
+        self.on_ground = False
         self.attacked, self.attack_time = False, 0
 
     def update(self):
-        if not clone1.on_ground:
-            self.rect.y += 6
-            fighter1.image = fighter1.images['flying']
+        if not jumping1:
+            if not self.on_ground:
+                self.image = self.images['flying']
+                self.falling += 0.25
+                self.rect.y += 6 + self.falling // 1
+                if pygame.sprite.spritecollide(self, stative_textures_sprites, False) \
+                        or pygame.sprite.spritecollide(self, moving_textures_sprites, False):
+                    self.rect.y -= 6 + self.falling // 1
+                    self.falling = 0
+                    self.on_ground = True
+            else:
+                self.rect.y += 6
+                if pygame.sprite.spritecollide(self, stative_textures_sprites, False) \
+                        or pygame.sprite.spritecollide(self, moving_textures_sprites, False):
+                    self.rect.y -= 6
+                else:
+                    self.image = self.images['flying']
+                    self.on_ground = False
 
 
 class Fighter2(pygame.sprite.Sprite):
@@ -103,75 +121,34 @@ class Fighter2(pygame.sprite.Sprite):
 
     def __init__(self, group):
         super().__init__(group)
+        self.falling = 0
         self.image = Fighter1.images['standing'][0]
         self.rect = self.image.get_rect()
         self.width, self.height = self.rect[2], self.rect[3]
         self.rect.x, self.rect.y = 810, 100
         self.direction = 'left'
+        self.on_ground = False
         self.attacked, self.attack_time = False, 0
 
     def update(self):
-        if not clone2.on_ground:
-            self.rect.y += 6
-            fighter2.image = fighter2.images['flying']
-
-
-class Clone1(pygame.sprite.Sprite):
-    images = {
-        'standing': load_image('fighter1', 'standing1.png')
-    }
-
-    def __init__(self, group):
-        super().__init__(group)
-        self.image = Clone1.images['standing']
-        self.rect = self.image.get_rect()
-        self.width, self.height = self.rect[2], self.rect[3]
-        self.rect.x, self.rect.y = 100, 100
-        self.on_ground = False
-
-    def update(self):
-        if not self.on_ground:
-            self.rect.y += 6
-            if pygame.sprite.spritecollide(self, stative_textures_sprites, False) \
-                    or pygame.sprite.spritecollide(self, moving_textures_sprites, False):
-                self.rect.y -= 6
-                self.on_ground = True
-        else:
-            self.rect.y += 6
-            if pygame.sprite.spritecollide(self, stative_textures_sprites, False) \
-                    or pygame.sprite.spritecollide(self, moving_textures_sprites, False):
-                self.rect.y -= 6
+        if not jumping2:
+            if not self.on_ground:
+                self.image = self.images['flying']
+                self.falling += 0.25
+                self.rect.y += 6 + self.falling // 1
+                if pygame.sprite.spritecollide(self, stative_textures_sprites, False) \
+                        or pygame.sprite.spritecollide(self, moving_textures_sprites, False):
+                    self.rect.y -= 6 + self.falling // 1
+                    self.falling = 0
+                    self.on_ground = True
             else:
-                self.on_ground = False
-
-
-class Clone2(pygame.sprite.Sprite):
-    images = {
-        'standing': load_image('fighter2', 'standing1.png')
-    }
-
-    def __init__(self, group):
-        super().__init__(group)
-        self.image = Clone2.images['standing']
-        self.rect = self.image.get_rect()
-        self.width, self.height = self.rect[2], self.rect[3]
-        self.rect.x, self.rect.y = 810, 100
-        self.on_ground = False
-
-    def update(self):
-        if not self.on_ground:
-            self.rect.y += 6
-            if pygame.sprite.spritecollide(self, stative_textures_sprites, False) \
-                    or pygame.sprite.spritecollide(self, moving_textures_sprites, False):
-                self.rect.y -= 6
-                self.on_ground = True
-        else:
-            self.rect.y += 6
-            if pygame.sprite.spritecollide(self, stative_textures_sprites, False) \
-                    or pygame.sprite.spritecollide(self, moving_textures_sprites, False):
-                self.rect.y -= 6
-            else:
-                self.on_ground = False
+                self.rect.y += 6
+                if pygame.sprite.spritecollide(self, stative_textures_sprites, False) \
+                        or pygame.sprite.spritecollide(self, moving_textures_sprites, False):
+                    self.rect.y -= 6
+                else:
+                    self.image = self.images['flying']
+                    self.on_ground = False
 
 
 class MovingTexture(pygame.sprite.Sprite):
@@ -198,9 +175,6 @@ stative_textures_sprites = pygame.sprite.Group()
 moving_textures_sprites = pygame.sprite.Group()
 unused_sprites = pygame.sprite.Group()
 
-clone1 = Clone1(unused_sprites)
-clone2 = Clone2(unused_sprites)
-
 box1 = MovingTexture(stative_textures_sprites, 150, 425, 'medium_box.png')
 box2 = MovingTexture(stative_textures_sprites, 350, 400, 'big_box.png')
 box3 = MovingTexture(stative_textures_sprites, 600, 450, 'small_box.png')
@@ -216,7 +190,14 @@ fighter_sprites = pygame.sprite.Group()
 fighter1 = Fighter1(fighter_sprites)
 fighter2 = Fighter2(fighter_sprites)
 
-counter, running, clock, fps = 0, True, pygame.time.Clock(), 110
+counter, running, clock, fps = 0, True, pygame.time.Clock(), 60
+
+jumping1 = False
+jumping2 = False
+jumped1 = 20
+jumped2 = 20
+jj1 = 10
+jj2 = 10
 
 while running:
     counter += 1
@@ -227,47 +208,75 @@ while running:
 
     pressed = pygame.key.get_pressed()
 
+    if jumping1:
+        jumped1 -= 1
+        jj1 -= 0.5
+        fighter1.rect.y -= jj1 // 1
+        if pygame.sprite.spritecollide(fighter1, stative_textures_sprites, False) \
+                or pygame.sprite.spritecollide(fighter1, moving_textures_sprites, False):
+            fighter1.rect.y += jj1 // 1
+            jumped1 = 20
+            jj1 = 10
+            jumping1 = False
+        if jumped1 == 0:
+            jumped1 = 20
+            jj1 = 10
+            jumping1 = False
+
+    if jumping2:
+        jumped2 -= 1
+        jj2 -= 0.5
+        fighter2.rect.y -= jj2 // 1
+        if pygame.sprite.spritecollide(fighter2, stative_textures_sprites, False) \
+                or pygame.sprite.spritecollide(fighter2, moving_textures_sprites, False):
+            fighter2.rect.y += jj2 // 1
+            jumped2 = 20
+            jj2 = 10
+            jumping2 = False
+        if jumped1 == 0:
+            jumped2 = 20
+            jj2 = 10
+            jumping2 = False
+
     if pressed[pygame.K_d]:
-        fighter1.direction = 'right'
-        clone1.rect.x += 2
-        if not counter % 10:
-            if fighter1.image == fighter1.images['running'][0]:
-                fighter1.image = fighter1.images['running'][1]
-            else:
-                fighter1.image = fighter1.images['running'][0]
-        if pygame.sprite.spritecollide(clone1, stative_textures_sprites, False) \
-                or pygame.sprite.spritecollide(clone1, moving_textures_sprites, False):
-            clone1.rect.x -= 2
-        else:
-            fighter1.rect.x += 2
+        if not jumping1:
+            fighter1.direction = 'right'
+            if not counter % 10:
+                if fighter1.image == fighter1.images['running'][0]:
+                    fighter1.image = fighter1.images['running'][1]
+                else:
+                    fighter1.image = fighter1.images['running'][0]
+
+        fighter1.rect.x += 5
+        if pygame.sprite.spritecollide(fighter1, stative_textures_sprites, False) \
+                or pygame.sprite.spritecollide(fighter1, moving_textures_sprites, False):
+            fighter1.rect.x -= 5
 
     if pressed[pygame.K_a]:
-        fighter1.direction = 'left'
-        clone1.rect.x -= 2
-        if not counter % 10:
-            if fighter1.image == fighter1.images['running'][2]:
-                fighter1.image = fighter1.images['running'][3]
-            else:
-                fighter1.image = fighter1.images['running'][2]
-        if pygame.sprite.spritecollide(clone1, stative_textures_sprites, False) \
-                or pygame.sprite.spritecollide(clone1, moving_textures_sprites, False):
-            clone1.rect.x += 2
-        else:
-            fighter1.rect.x -= 2
+        if not jumping1:
+            fighter1.direction = 'left'
+            if not counter % 10:
+                if fighter1.image == fighter1.images['running'][2]:
+                    fighter1.image = fighter1.images['running'][3]
+                else:
+                    fighter1.image = fighter1.images['running'][2]
 
-    if not pressed[pygame.K_d] and not pressed[pygame.K_a]:
+        fighter1.rect.x -= 5
+        if pygame.sprite.spritecollide(fighter1, stative_textures_sprites, False) \
+                or pygame.sprite.spritecollide(fighter1, moving_textures_sprites, False):
+            fighter1.rect.x += 5
+
+    if not pressed[pygame.K_d] and not pressed[pygame.K_a] and not jumping1 and fighter2.on_ground:
         if fighter1.direction == 'left':
             fighter1.image = fighter1.images['standing'][1]
         else:
             fighter1.image = fighter1.images['standing'][0]
 
     if pressed[pygame.K_w]:
-        clone1.rect.y -= 12
-        if pygame.sprite.spritecollide(clone1, stative_textures_sprites, False) \
-                or pygame.sprite.spritecollide(clone1, moving_textures_sprites, False):
-            clone1.rect.y += 12
-        else:
-            fighter1.rect.y -= 12
+        if fighter1.on_ground:
+            fighter1.image = fighter1.images['flying']
+            fighter1.on_ground = False
+            jumping1 = True
 
     if pressed[pygame.K_s]:
         if fighter1.attack_time >= 250:
@@ -287,46 +296,44 @@ while running:
         fighter1.attack_time += 1
 
     if pressed[pygame.K_RIGHT]:
-        fighter2.direction = 'right'
-        clone2.rect.x += 2
-        if not counter % 10:
-            if fighter2.image == fighter2.images['running'][2]:
-                fighter2.image = fighter2.images['running'][3]
-            else:
-                fighter2.image = fighter2.images['running'][2]
-        if pygame.sprite.spritecollide(clone2, stative_textures_sprites, False) \
-                or pygame.sprite.spritecollide(clone2, moving_textures_sprites, False):
-            clone2.rect.x -= 2
-        else:
-            fighter2.rect.x += 2
+        if not jumping2:
+            fighter2.direction = 'right'
+            if not counter % 10:
+                if fighter2.image == fighter2.images['running'][2]:
+                    fighter2.image = fighter2.images['running'][3]
+                else:
+                    fighter2.image = fighter2.images['running'][2]
+
+        fighter2.rect.x += 5
+        if pygame.sprite.spritecollide(fighter2, stative_textures_sprites, False) \
+                or pygame.sprite.spritecollide(fighter2, moving_textures_sprites, False):
+            fighter2.rect.x -= 5
 
     if pressed[pygame.K_LEFT]:
-        fighter2.direction = 'left'
-        clone2.rect.x -= 2
-        if not counter % 10:
-            if fighter2.image == fighter2.images['running'][0]:
-                fighter2.image = fighter2.images['running'][1]
-            else:
-                fighter2.image = fighter2.images['running'][0]
-        if pygame.sprite.spritecollide(clone2, stative_textures_sprites, False) \
-                or pygame.sprite.spritecollide(clone2, moving_textures_sprites, False):
-            clone2.rect.x += 2
-        else:
-            fighter2.rect.x -= 2
+        if not jumping2:
+            fighter2.direction = 'left'
+            if not counter % 10:
+                if fighter2.image == fighter2.images['running'][0]:
+                    fighter2.image = fighter2.images['running'][1]
+                else:
+                    fighter2.image = fighter2.images['running'][0]
 
-    if not pressed[pygame.K_RIGHT] and not pressed[pygame.K_LEFT]:
+        fighter2.rect.x -= 5
+        if pygame.sprite.spritecollide(fighter2, stative_textures_sprites, False) \
+                or pygame.sprite.spritecollide(fighter2, moving_textures_sprites, False):
+            fighter2.rect.x += 5
+
+    if not pressed[pygame.K_RIGHT] and not pressed[pygame.K_LEFT] and not jumping2 and fighter2.on_ground:
         if fighter2.direction == 'left':
             fighter2.image = fighter2.images['standing'][0]
         else:
             fighter2.image = fighter2.images['standing'][1]
 
     if pressed[pygame.K_UP]:
-        clone2.rect.y -= 12
-        if pygame.sprite.spritecollide(clone2, stative_textures_sprites, False) \
-                or pygame.sprite.spritecollide(clone2, moving_textures_sprites, False):
-            clone2.rect.y += 12
-        else:
-            fighter2.rect.y -= 12
+        if fighter2.on_ground:
+            fighter2.image = fighter2.images['flying']
+            fighter2.on_ground = False
+            jumping2 = True
 
     if pressed[pygame.K_DOWN]:
         if fighter2.attack_time >= 250:
